81dff7f50ac39d7d306802e26a9002e776e40197
Derek Mackley <derek.mackley@hotmail.com>
2025-03-26 10:59:27 -0500

    remove config check testing, configmaps

--- apps/posterizarr/entrypoint.sh
+++ apps/posterizarr/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-## Check if config.json exists
-if [ -f "/config/config.json" ]; then
-    chmod 644 /config/config.json
-fi
-
 # Remove running file if it exists
 rm /config/temp/Posterizarr.Running || true
 
